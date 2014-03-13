f2a283478b953bec45578c58d026667ea8127521
Python1320 <python1320@gmail.com>
2014-03-13 18:58:29 +0200

    initial plan

---
+++ lua/luapack/autoloader.lua
@@ -0,0 +1,18 @@
+local files,_ = file.Find("autorun/*.lua",'LUA')
+
+local files,_ = file.Find("autorun/client/*.lua",'LUA')
+
+local files,_ = file.Find("entities/*.lua",'LUA')
+
+local _,folders = file.Find("entities/*",'LUA')
+-- cl_init, shared
+
+local _,folders = file.Find("weapons/*",'LUA')
+-- cl_init, shared
+
+local files,_ = file.Find("effects/*.lua",'LUA')
+
+local _,folders = file.Find("effects/*",'LUA')
+-- init.lua
+
+
