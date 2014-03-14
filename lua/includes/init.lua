9fabf3604473ddc9fbd03c92cd8315ed6abf2700
Python1320 <python1320@gmail.com>
2014-03-15 00:05:40 +0200

    Test hook

--- lua/includes/init.lua
+++ lua/includes/init.lua
@@ -12,4 +12,10 @@ else
 
 	include ( "luapack/cl_core.lua" )
 
+	hook.Add("CreateTeams","asd",function()
+		print("CreateTeams>",gmod.GetGamemode(),GAMEMODE,GM,engine.ActiveGamemode())
+	end)
+	hook.Add("PreGamemodeLoaded","asd",function()
+		print("PreGamemodeLoaded>",gmod.GetGamemode(),GAMEMODE,GM,engine.ActiveGamemode())
+	end)
 end
